b579fd617b6f6ab58e704fbf78c3ff596d80b989
atlasgrowth <nicholas@atlasgrowth.ai>
2025-02-16 17:02:18 +0000

    Restored to 'ff95f69532fc566372c979af6c8a8138b500a1d9'
    Replit-Restored-To:ff95f69532fc566372c979af6c8a8138b500a1d9

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,15 +5,11 @@ import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import Dashboard from "@/pages/dashboard";
 import BusinessDetails from "@/pages/business-details";
-import Home from "@/pages/home";
-import EmailDashboard from "@/pages/email";
 
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/pipeline" component={Dashboard} />
-      <Route path="/email" component={EmailDashboard} />
+      <Route path="/" component={Dashboard} />
       <Route path="/business/:siteId" component={BusinessDetails} />
       <Route component={NotFound} />
     </Switch>
